806bd55b34b8ed748a286249065709ffbd21670e
AndrejTesten <andrej.testen33@gmail.com>
2026-01-26 20:01:56 +0100

    Test final

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'; // ✅ import
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(routes),
-    importProvidersFrom(BrowserAnimationsModule)
+    importProvidersFrom(BrowserAnimationsModule),
+    { provide: LocationStrategy, useClass: HashLocationStrategy } // ✅ enable hash routing
   ]
 }).catch(err => console.error(err));
